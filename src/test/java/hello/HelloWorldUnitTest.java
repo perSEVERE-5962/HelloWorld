7074b1ad37b1058e3100b5d4bf03099248e623c8
Unknown <gdevisetti@gmail.com>
2018-12-19 17:57:11 -0500

    initial commit

---
+++ src/test/java/hello/HelloWorldUnitTest.java
@@ -0,0 +1,12 @@
+package hello;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldUnitTest {
+    @Test
+    public void isHelloWorldTest() {
+        assertEquals("HelloWorld!", HelloWorld.getText());
+    }
+}
+
+
