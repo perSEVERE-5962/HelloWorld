833b79d497b64f63688facb5ff677dca4edd642c
Unknown <gdevisetti@gmail.com>
2018-12-30 10:56:21 -0500

    recovering files

---
+++ src/main/java/utils/LimitSwitch.java
@@ -0,0 +1,16 @@
+package utils;
+//import com.pi4j.io.gpio;
+
+public class LimitSwitch{
+    public static void main (String[] args){
+        // gpio.setmode(gpio.BCM);
+        // gpio.setup(18, gpio.IN, pull_down = gpio.pull_down);
+        // while (True){
+        //     input_state = gpio.input(18);
+        //     if (input_state == false){
+        //         System.out.println("Button is pressed");
+        //     }
+        // }
+    }
+
+}
